b805341b4b1e3c09c814813b0f37f427200f2f7b
Emilie Xu <31358826+emilie-xu@users.noreply.github.com>
2023-05-08 05:01:49 -0400

    changed add_favorites to use toggle

--- add_favorites_completed/lambda_function.py
+++ add_favorites_completed/lambda_function.py
@@ -1,78 +1,75 @@
-import json
-import boto3
-from requests_aws4auth import AWS4Auth
-from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
-#from opensearchpy import OpenSearch, RequestsHttpConnection
-import requests
-from decimal import Decimal
-import datetime
-import collections
-from urllib.parse import quote
-import sys
-if sys.version_info.major == 3 and sys.version_info.minor >= 10:
-    from collections.abc import MutableMapping
-else:
-    from collections import MutableMapping
-import time
+'''
+input:
+{email: str,
+workoutid: str,
+toggleDone: bool,
+toggleFavorite: bool}
 
-region = 'us-east-1'
-service = 'es'
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+output:
+{email: str,
+workoutid: str,
+done: bool,
+favorite: bool,
+doneChanged: bool,
+favChanged: bool
+}
+'''
+import json
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('user-profile')
 
-#userid, favoritedids, doneids
-
-def flatten(d, parent_key='', sep='_'):
-    items = []
-    for key, val in d.items():
-        new_key = key
-        if isinstance(val, collections.abc.MutableMapping):
-            items.extend(flatten(val, new_key, sep=sep).items())
-        else:
-            items.append((new_key, val))
-    return dict(items)
-
-def floats_to_str(item,float_vars):
-    for var in item:
-        if var in float_vars:
-            item[var]=Decimal(str(item[var]))
-    return item
-
 dict_list = []
 var_list = ['userid', 'favoritedids', 'doneids']
 
-def updateItems(workout, in_data, location_setting):
-    #assuming input data in form (userid str, workoutid str, done bool, favorite bool)
+def updateItems(in_data):
+    #assuming input data in form (userid str, workoutid str, toggleDone bool, toggleFavorite bool)
     #print(in_data)
     try:
         rec2 = {}
-        rec2['userid'] = in_data['userid']
+        email = in_data['email']
+        rec2['email'] = email
         
         #NEED TO DO ERROR CHECKING FOR UNAVAILABLE
-        userdata = table.get_item(
-            Key={'id': userid}
-            )
+        userdata = table.get_item(Key={"email": email})['Item']
+        
+        print(userdata)
         
         favs = userdata['favoritedids']
         dones = userdata['doneids']
+        wid = in_data['workoutid']
         
-        if(in_data['favorite']==True):
-            favs.append(in_data['workoutid'])
-        if(in_data['done']==True):
-            dones.append(in_data['workoutid'])
-            
-        rec2['favoritedids'] = favs
-        rec2['doneids'] = dones
-        rec2['latestUpdateTime'] = str(datetime.datetime.now())
+        toggleDone = in_data['done']
+        toggleFavorite = in_data['favorite']
+        
+        done = wid in dones
+        favorite = wid in favs
+        
+        if(toggleFavorite):
+            if(wid not in favs):
+                favs.append(wid)
+                favorite = True
+            else:
+                favs.remove(wid)
+                favorite = False
+        if(toggleDone):
+            if(wid not in dones):
+                dones.append(wid)
+                done = True
+            else:
+                dones.remove(wid)
+                done = False
+
+        userdata['favoritedids'] = favs
+        userdata['doneids'] = dones
+        userdata['latestUpdateTime'] = str(datetime.datetime.now())
         
         #dict_list.append(rec2)
+        print(userdata)
         
-        r = table.update_item(Item=rec2)
-        #print('put record succeeded:', r)
-        time.sleep(0.001)
+        r = table.put_item(Item=userdata)#, Key=email)
+        print('put record succeeded:', r)
+        return favorite, done
     except Exception as e:
         print('add record failed: ', in_data)
         print(e)
@@ -86,13 +83,19 @@ def lambda_handler(event, context):
     #EXTRACT USER DATA OF FORM (userid str, workoutid str, done bool, favorite bool)
     
     userdataupdates = event#EXTRACT
-    updateItems(userdataupdates)
-
+    fav, done = updateItems(userdataupdates) 
+    
+    userdataupdates['favChanged'] = userdataupdates['favorite']
+    userdataupdates['doneChanged'] = userdataupdates['done']
+    userdataupdates['favorite'] = fav
+    userdataupdates['done'] = done
+    print(userdataupdates)
+    
     return {
         'status': 200,
         'headers': {
             "Access-Control-Allow-Origin": "*",
             'Content-Type': 'application/json'
         },
-        'body': 'successfully updated favorite and done data'
+        'body': json.dumps(userdataupdates)
     }
